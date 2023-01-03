b7e81a842fefde1a35eaae0fa30ba7b73e542699
snehal patel <snpatel.dev@gmail.com>
2023-01-02 22:22:41 -0500

    #2 implementing search bar

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent {
+  @Output() submitted = new EventEmitter<string>();
+  term = '';
 
+  onFormSubmit(event: Event) {
+    event.preventDefault();
+    this.submitted.emit(this.term);
+  }
 }
