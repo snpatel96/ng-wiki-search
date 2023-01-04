92056a7868e67ae480fc169b342b165b359703f3
snehal patel <snpatel.dev@gmail.com>
2023-01-04 17:29:14 -0500

    #4 make http request and getting data

--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-page-list',
   templateUrl: './page-list.component.html',
-  styleUrls: ['./page-list.component.css']
+  styleUrls: ['./page-list.component.css'],
 })
 export class PageListComponent {
-
+  @Input() pages: any = [];
 }
--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -8,6 +8,15 @@ export class WikipediaService {
   constructor(private http: HttpClient) {}
 
   search(term: string) {
-    return 'I am wikipedia search result.';
+    return this.http.get('https://en.wikipedia.org/w/api.php', {
+      params: {
+        action: 'query',
+        format: 'json',
+        list: 'search',
+        utf8: '1',
+        srsearch: term,
+        origin: '*',
+      },
+    });
   }
 }
